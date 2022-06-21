c83797315ba79622a1da9dfe858f0072ef934850
Christine Mwangi <christine.mwangi@student.moringaschool.com>
2022-06-22 01:13:58 +0300

    ft:created neighbourhood model

--- hoodview/models.py
+++ hoodview/models.py
@@ -1,3 +1,11 @@
+from unicodedata import name
 from django.db import models
 
 # Create your models here.
+#1.Neighbourhood class,2.User class,3.Profile class,Busiiness,Post
+
+class Neighbourhood(models.Model):
+    name = models.CharField(max_length=60)
+    location = models.CharField(max_length=70)
+    description = models.TextField(max_length=255,help_text="Description of the Neighbourhood.")
+    health_tel = models.IntegerField(max_length=100,null=True,blank=True)
