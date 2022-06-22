4560a07fcee014e790c585de3a6823c02632b0ec
Christine Mwangi <christine.mwangi@student.moringaschool.com>
2022-06-22 10:04:49 +0300

    ft:created signup and homepage view functions

--- hoodview/views.py
+++ hoodview/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect,get_object_or_404
+from django.http import HttpResponse
+from django.contrib.auth import login, authenticate
+from .forms import BusinessForm,SignupForm
+from .models import Business,Post,Neighbourhood,Profile
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+
+def index(request):
+    return render(request, 'index.html')
+
+
+def signup(request):
+    if request.method == 'POST':
+        form=SignupForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            return redirect('index')
+    else:
+        form = SignupForm()
+    return render(request, 'registration/signup.html', {'form': form})
