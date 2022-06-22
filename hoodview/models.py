9439a5054668e24528801ed4c219eaa94eb3236d
Christine Mwangi <christine.mwangi@student.moringaschool.com>
2022-06-22 09:21:48 +0300

    ft:created a profile class

--- hoodview/models.py
+++ hoodview/models.py
@@ -1,11 +1,23 @@
-from unicodedata import name
 from django.db import models
+from django.contrib.auth.urls import User
 
 # Create your models here.
-#1.Neighbourhood class,2.User class,3.Profile class,Busiiness,Post
+#1.Neighbourhood class,.Profile class,Busiiness,Post
 
 class Neighbourhood(models.Model):
     name = models.CharField(max_length=60)
     location = models.CharField(max_length=70)
     description = models.TextField(max_length=255,help_text="Description of the Neighbourhood.")
     health_tel = models.IntegerField(max_length=100,null=True,blank=True)
+    police_tel = models.IntegerField(max_length=100,null=True)
+    
+    
+    
+    
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    name = models.CharField(max_length=80, blank=True)
+    bio = models.TextField(max_length=240, blank=True)
+    profile_picture = models.ImageField(upload_to='images/', default='.png')
+    location = models.CharField(max_length=50, blank=True, null=True)
+    neighbourhood = models.ForeignKey(Neighbourhood, on_delete=models.SET_NULL, null=True, related_name='members', blank=True)
