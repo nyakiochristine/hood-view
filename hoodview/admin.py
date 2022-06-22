073e25fc6d0f6ef4fccd2c7fc1960d53bc851e76
Christine Mwangi <christine.mwangi@student.moringaschool.com>
2022-06-22 10:34:55 +0300

    ft:registered models

--- hoodview/admin.py
+++ hoodview/admin.py
@@ -1,3 +1,11 @@
+from re import A
 from django.contrib import admin
+from .models import Profile,Post,Business,Neighbourhood
+
+
+admin.site.register(Profile)
+admin.site.register(Business)
+admin.site.register(Neighbourhood)
+admin.site.register(Post)
 
 # Register your models here.
