d8d8caeddb7246490e3b08d9728911a654ef8cdd
Christine Mwangi <christine.mwangi@student.moringaschool.com>
2022-06-23 01:27:21 +0300

    ft:created test for the neirghbourhood model

--- hoodview/tests.py
+++ hoodview/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+
+class Neighbourhood(TestCase):
+    name = 'hoodview'
+    location = 'Nairobi'
+    admin = 'Kinuthia'
+    description = 'This is a beautiful estate'
+    health_tel = '25729'
+    police_tel = '911'
+    
+    
