faa4fed1c1c4b46996a6b78971f6d95c982a3b85
Christine Mwangi <christine.mwangi@student.moringaschool.com>
2022-06-23 01:21:02 +0300

    ft:created functions for model classes

--- hoodview/models.py
+++ hoodview/models.py
@@ -1,8 +1,12 @@
-import email
-from unicodedata import name
+from distutils.command.upload import upload
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 from django.contrib.auth.models import User
+from pyuploadcare.dj.models import ImageField
 
+from django.contrib.auth.models import User
+#from cloudinary import CloudinaryImage
 # Create your models here.
 #1.Neighbourhood class,.Profile class,Busiiness,Post
 
@@ -12,7 +16,7 @@ class Neighbourhood(models.Model):
     admin = models.CharField(max_length=30)
     description = models.TextField(max_length=255,help_text="Description of the Neighbourhood.")
     health_tel = models.IntegerField(null=True,blank=True)
-    police_tel = models.IntegerField(null=True)
+    police_tel = models.IntegerField(null=True,blank=True)
     
     def __str__(self):
         return f'{self.name} hood'
@@ -37,16 +41,19 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
     name = models.CharField(max_length=80, blank=True)
     bio = models.TextField(max_length=240, blank=True)
-    profile_picture = models.ImageField(upload_to='images/', default='.png')
+    profile_picture = models.ImageField(upload_to='images/', default='default.png')
     location = models.CharField(max_length=50, blank=True, null=True)
     neighbourhood = models.ForeignKey(Neighbourhood, on_delete=models.SET_NULL, null=True, related_name='members', blank=True)
     
+    def __str__(self):
+        return f'{self.user.username} profile'
     
+    @receiver(post_save, sender=User)
     def create_user_profile(sender, instance, created, **kwargs):
         if created:
             Profile.objects.create(user=instance)
 
-    
+    @receiver(post_save, sender=User)
     def save_user_profile(sender, instance, **kwargs):
         instance.profile.save()
     
