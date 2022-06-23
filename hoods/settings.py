47406a197fd3a64af4857674df31040fb83a84c8
Christine Mwangi <christine.mwangi@student.moringaschool.com>
2022-06-23 10:17:45 +0300

    fixed bugs

--- hoods/settings.py
+++ hoods/settings.py
@@ -117,7 +117,7 @@ else:
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
-ALLOWED_HOSTS='hood234.herokuapp.com'
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
 
 
